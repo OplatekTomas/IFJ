26d08703d4e7dfec1c5fa631c6c9e17aa55c1627
Petr Volf <Petr.volf216@gmail.com>
2019-10-11 22:31:41 +0200

    krehke urcovani indentu

--- src/main.c
+++ src/main.c
@@ -1,9 +1,67 @@
 #include <stdio.h>
-#include "test.h"
+#include <stdlib.h>
+#include "stack.h"
+
+int count_spaces(FILE* in) {
+    int spaces = 0;
+    char c;
+    do {
+        c = (char)getc(in);
+        if (c == ' ') {
+            spaces++;
+        } else {
+            ungetc(c, in);
+            break;
+        }
+    } while (true);
+    return spaces;
+}
 
 int main () {
-    printf("Hello there!\n");
-    test();
-    printf("FUCK");
+    char c;
+    IndentStack is;
+    stack_init(&is);
+    // cteni souboru znak po znaku
+    do {
+        c = (char)getc(stdin);
+        switch (c) {
+            case '\n':;
+                // je novy radek
+                // multiline stringy rozbijou rozpoznavani indentu
+                int indent = count_spaces(stdin);
+                // je jiny indent nez na topu
+                if (indent != stack_top(&is)) {
+                    // pridani indentu
+                    if (indent > stack_top(&is)) {
+                        stack_push(&is, indent);
+                        printf("INDENT\n");
+                    } else if (indent < stack_top(&is)) {
+                        // ubrani indentu
+                        do {
+                            stack_pop(&is);
+                            printf("DEDENT\n");
+                            // jsme na spravne urovni dedentu?
+                            if (stack_top(&is) == indent) {
+                                break;
+                            } else if (stack_top(&is) < indent) {
+                                // indent muze byt mensi nez top jedine v pripade ze se popovalo a nenasla se hodnota topu, takze nastala indent chyba
+                                exit(1);
+                            }
+                        } while (stack_top(&is) != 0);
+                    }
+                }
+                break;
+            case '"':;
+                break;
+            default:
+                break;
+        }
+    } while (c != EOF);
+
+    // pridani dedentu na konci pro vyrovani s indentama
+    while (stack_top(&is) != 0) {
+        stack_pop(&is);
+        printf("DEDENT\n");
+    }
     return 0;
 }
---
+++ src/stack.h
@@ -0,0 +1,19 @@
+#ifndef IFJ_STACK_H
+#define IFJ_STACK_H
+
+#include <stdbool.h>
+
+#define STACK_MAX_SIZE 30
+
+typedef struct {
+    unsigned level;
+    int data[STACK_MAX_SIZE];
+} IndentStack;
+
+void stack_init(IndentStack* is);
+void stack_pop(IndentStack* is);
+int stack_top(IndentStack* is);
+void stack_push(IndentStack* is, int value);
+bool stack_empty(IndentStack* is);
+
+#endif //IFJ_STACK_H
