6daef0f9d94495cac359818c03bca88fc198b8f1
David Rubý <dejfik@icloud.com>
2019-10-14 19:37:21 +0200

    syntax_analyzer

--- src/tokens.h
+++ src/tokens.h
@@ -1,4 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum tokens{ID, COMP, OPEN_PARENTHES, CLOSE_PARENTHES, INT, BOOL, FLOAT, STRING, NIL};
+enum tokens{ID, COMP, OPEN_PARENTHES, CLOSE_PARENTHES, INT, BOOL, FLOAT, STRING, NIL, END_OF_FILE};
