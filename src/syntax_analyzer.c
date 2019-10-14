20a6bc26aad7372c6aebfa91331aac25c87270a1
David Rubý <dejfik@icloud.com>
2019-10-14 20:27:50 +0200

    syntax_analyzer
    vytvořena funkce volající get_next_token

--- src/syntax_analyzer.c
+++ src/syntax_analyzer.c
@@ -1,3 +1,10 @@
 #include "syntax_analyzer.h"
 
+char token_type[10][100] = {"ID", "COMP", "OPEN_PARENTHES", "CLOSE_PARENTHES", "INT", "BOOL", "FLOAT", "STRING", "NIL", "END_OF_FILE"};
 
+void get_derivation_tree(FILE *source){
+    token t;
+    while((t = get_next_token(source)).type != END_OF_FILE){
+        printf("%c\n", token_type[t.type]);
+    };
+}
--- src/syntax_analyzer.h
+++ src/syntax_analyzer.h
@@ -1,4 +1,8 @@
 #ifndef IFJ_SYNTAX_ANALYZER_H
 #define IFJ_SYNTAX_ANALYZER_H
 
+#include "lex_analyzer.h"
+
+void get_derivation_tree(FILE *source);
+
 #endif //IFJ_SYNTAX_ANALYZER_H
