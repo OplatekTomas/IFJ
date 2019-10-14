3ebcad74723b72a8c11a8976b2f5ee0472b2c980
Tomáš Oplatek <tomas.oplatek@gmail.com>
2019-10-14 18:03:29 +0200

    Moved code from main

--- src/main.c
+++ src/main.c
@@ -1,67 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "stack.h"
+#include "lex_analyzer.h"
 
-int count_spaces(FILE* in) {
-    int spaces = 0;
-    char c;
-    do {
-        c = (char)getc(in);
-        if (c == ' ') {
-            spaces++;
-        } else {
-            ungetc(c, in);
-            break;
-        }
-    } while (true);
-    return spaces;
-}
+#define DEBUG 1
 
-int main () {
-    char c;
-    IndentStack is;
-    stack_init(&is);
-    // cteni souboru znak po znaku
-    do {
-        c = (char)getc(stdin);
-        switch (c) {
-            case '\n':;
-                // je novy radek
-                // multiline stringy rozbijou rozpoznavani indentu
-                int indent = count_spaces(stdin);
-                // je jiny indent nez na topu
-                if (indent != stack_top(&is)) {
-                    // pridani indentu
-                    if (indent > stack_top(&is)) {
-                        stack_push(&is, indent);
-                        printf("INDENT\n");
-                    } else if (indent < stack_top(&is)) {
-                        // ubrani indentu
-                        do {
-                            stack_pop(&is);
-                            printf("DEDENT\n");
-                            // jsme na spravne urovni dedentu?
-                            if (stack_top(&is) == indent) {
-                                break;
-                            } else if (stack_top(&is) < indent) {
-                                // indent muze byt mensi nez top jedine v pripade ze se popovalo a nenasla se hodnota topu, takze nastala indent chyba
-                                exit(1);
-                            }
-                        } while (stack_top(&is) != 0);
-                    }
-                }
-                break;
-            case '"':;
-                break;
-            default:
-                break;
-        }
-    } while (c != EOF);
-
-    // pridani dedentu na konci pro vyrovani s indentama
-    while (stack_top(&is) != 0) {
-        stack_pop(&is);
-        printf("DEDENT\n");
+int main (int argc, char *argv[]) {
+    if(DEBUG){
+        FILE *f = fopen(argv[1], "r");
+        get_next_token(f);
+        fclose(f);
+    }else{
+        get_next_token(stdin);
     }
+
     return 0;
 }
--- src/stack.c
+++ src/stack.c
@@ -1,26 +1,26 @@
 #include "stack.h"
 
-void stack_init(IndentStack* is) {
+void stack_init(indentStack* is) {
     is->level = 0;
     is->data[0] = 0;
 }
 
-void stack_pop(IndentStack* is) {
+void stack_pop(indentStack* is) {
     if (!stack_empty(is)) {
         is->level--;
     }
 }
 
-int stack_top(IndentStack *is) {
+int stack_top(indentStack *is) {
     return is->data[is->level];
 }
 
-void stack_push(IndentStack* is, int value) {
+void stack_push(indentStack* is, int value) {
     is->level++;
     is->data[is->level] = value;
 }
 
-bool stack_empty(IndentStack* is) {
+bool stack_empty(indentStack* is) {
     return is->level == 0;
 }
 
