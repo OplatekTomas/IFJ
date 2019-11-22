34354254a1e08f9d21d324aa7bbcf724c01b1182
Petr Volf <Petr.volf216@gmail.com>
2019-11-22 19:42:52 +0100

    par zmen

--- src/syntax_analyzer.c
+++ src/syntax_analyzer.c
@@ -31,131 +31,6 @@ char token_type[][100] = {
         "DOUBLE_DIV"
 };
 
-bool check_function_call(ASTNode* tree, Scanner* s) {
-    //TODO: dodelat
-    return false;
-}
-
-bool check_expression(ASTNode* tree, Scanner* s) {
-     printf("kontrola vyrazu\n");
-    // TODO: dodelat
-}
-
-bool check_assignment(ASTNode* tree, Scanner* s, char* left_side) {
-    //TODO: dodelat
-    printf("kontrola prirazeni\n");
-    if (check_expression(tree, s)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-bool check_if(ASTNode* tree, Scanner* s) {
-    //TODO: dodelat
-    return false;
-}
-
-bool check_while(ASTNode* tree, Scanner* s) {
-    //TODO: dodelat
-    return false;
-}
-
-bool check_definition(ASTNode* tree, Scanner* s) {
-    //TODO: dodelat
-    Token t = get_next_token(s);
-    if (t.type != ID) {
-
-    }
-
-    return false;
-}
-
-
-// TODO: definice funkce nemuze byt v ifu / whilu
-
-/// Vraci   0 - kdyz nastala lexikalni chyba
-///         1 - kdyz nastala syntakticka chyba
-///         2 - kdyz nenastala chyba
-///         3 - kdyz nastal konec souboru
-///         4 - kdyz narazi na 'DEDENT'
-int check_block(ASTNode* tree, Scanner* s) {
-    Token t = get_next_token(s);
-
-    printf("kontrola bloku\n");
-
-    switch (t.type) {
-        case ID:;
-            char* id = t.stringValue;
-            t = get_next_token(s);
-            switch (t.type) {
-                case ASSIGN:
-                    return check_assignment(tree, s, id);
-                case OPEN_PARENTHES:
-                    return check_function_call(tree, s);
-                default:
-                    return 1;
-            }
-        case KEYWORD:
-            switch (t.keywordValue) {
-                case IF:
-                    return check_if(tree, s);
-                case WHILE:
-                    return check_while(tree, s);
-                default:
-                    return 1;
-            }
-        case STRING:
-            //TODO: Poresit multiline stringy
-            return 1;
-        case END_OF_FILE:
-            return 3;
-        case DEDENT:
-            return 4;
-        default:
-            return 1;
-    }
-}
-
-int check_root_block(ASTNode* tree, Scanner *s) {
-    Token t = get_next_token(s);
-    switch (t.type) {
-        case KEYWORD:
-            if (t.keywordValue  == DEF) {
-                check_definition(tree, s);
-            }
-        default:
-            scanner_unget(s, t);
-            check_block(tree, s);
-    }
-}
-
-/// Vraci derivacni strom, ktery je potom potreba uvolnit
-ASTNode* get_derivation_tree(FILE *source) {
-    Scanner s;
-    scanner_init(&s, source);
-
-    ASTNode* root = (ASTNode*)malloc(sizeof(ASTNode));
-
-    while (true) {
-        int result = check_root_block(root ,&s);
-        switch (result) {
-            case 0:
-                fprintf(stderr, "nastala lexikalni chyba\n");
-                free_tree(root);
-                return NULL;
-            case 1:
-                fprintf(stderr, "nastala syntakticka chyba\n");
-                free_tree(root);
-                return NULL;
-            case 3:
-                break;
-            default:
-                continue;
-        }
-    }
-}
-
 void free_tree(ASTNode* tree) {
     // TODO: dodelat
     free(tree);
@@ -175,4 +50,140 @@ void node_insert(ASTNode* node, ASTNode new) {
     }
     node->nodes[node->subnode_len] = new;
     node->subnode_len += 1;
+}
+
+bool check_function_call(ASTNode* tree, Scanner* s) {
+    //TODO: dodelat
+    return true;
+}
+
+bool check_expression(ASTNode* tree, Scanner* s) {
+     printf("kontrola vyrazu\n");
+
+    // TODO: dodelat
+    while(true) {
+         Token t = get_next_token(s);
+         if (t.type == ID || t.type == FLOAT || t.type == INT || t.type == STRING || t.type == OPEN_PARENTHES || t.type == CLOSE_PARENTHES || t.type == COMMA) {
+
+        } else {
+            return false;
+        }
+     }
+}
+
+bool check_assignment(ASTNode* tree, Scanner* s, char* left_side) {
+    //TODO: dodelat
+    printf("kontrola prirazeni\n");
+    if (check_expression(tree, s)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool check_if(ASTNode* tree, Scanner* s) {
+    //TODO: dodelat
+    return true;
+}
+
+bool check_while(ASTNode* tree, Scanner* s) {
+    //TODO: dodelat
+    return true;
+}
+
+bool check_definition(ASTNode* tree, Scanner* s) {
+    //TODO: dodelat
+    Token t = get_next_token(s);
+    if (t.type != ID) {
+
+    }
+
+    return true;
+}
+
+
+// TODO: definice funkce nemuze byt v ifu / whilu
+
+/// Vraci   0 - kdyz nenastala chyba
+///         1 - kdyz nastala lexikalni chyba
+///         2 - kdyz nastala syntakticka chyba
+///         3 - kdyz nastal konec souboru
+///         4 - kdyz narazi na 'DEDENT'
+int check_block(ASTNode* tree, Scanner* s) {
+    Token t = get_next_token(s);
+
+    printf("kontrola bloku\n");
+
+    switch (t.type) {
+        case ID:;
+            char* id = t.stringValue;
+            t = get_next_token(s);
+            switch (t.type) {
+                case ASSIGN:
+                    return check_assignment(tree, s, id);
+                case OPEN_PARENTHES:
+                    return check_function_call(tree, s);
+                default:
+                    return 2;
+            }
+        case KEYWORD:
+            switch (t.keywordValue) {
+                case IF:
+                    return check_if(tree, s);
+                case WHILE:
+                    return check_while(tree, s);
+                default:
+                    return 2;
+            }
+        case STRING:
+            //TODO: Poresit multiline stringy
+            return 2;
+        case END_OF_FILE:
+            return 3;
+        case DEDENT:
+            return 4;
+        default:
+            return 2;
+    }
+}
+
+int check_root_block(ASTNode* tree, Scanner *s) {
+    Token t = get_next_token(s);
+    switch (t.type) {
+        case KEYWORD:
+            if (t.keywordValue  == DEF) {
+                return check_definition(tree, s);
+            }
+        default:
+            scanner_unget(s, t);
+            return check_block(tree, s);
+    }
+}
+
+/// Vraci derivacni strom, ktery je potom potreba uvolnit
+ASTNode* get_derivation_tree(FILE *source) {
+    Scanner s;
+    scanner_init(&s, source);
+
+    ASTNode* root = (ASTNode*)malloc(sizeof(ASTNode));
+    node_init(root);
+    root->node_type = PROGRAM_ROOT;
+
+    while (true) {
+        int result = check_root_block(root ,&s);
+        switch (result) {
+            case 1:
+                fprintf(stderr, "nastala lexikalni chyba\n");
+                free_tree(root);
+                return NULL;
+            case 2:
+                fprintf(stderr, "nastala syntakticka chyba\n");
+                free_tree(root);
+                return NULL;
+            case 3:
+                break;
+            default:
+                continue;
+        }
+    }
 }
