8ec6e9f5b91f9dd7953360bf9ff7be5ae951e8ad
David Rubý <dejfik@icloud.com>
2019-12-09 19:46:26 +0100

    zatím nic extra

--- src/code_gen.c
+++ src/code_gen.c
@@ -4,6 +4,37 @@
 
 #include "code_gen.h"
 
+void handle_next_block(ASTNode* tree, SymTable** table){
+    switch(tree->node_type){
+        case EXPRESSION:
+            generate_expression(tree, table);
+            break;
+        case IF_ELSE:
+            generate_if_else(tree, table);
+            break;
+        case WHILE_LOOP:
+            generate_while_loop(tree, table);
+            break;
+        case FUNCTION_DEFINITION:
+            generate_definition(tree, table);
+            break;
+        case FUNCITON_CALL:
+            generate_func_call(tree, table);
+            break;
+    }
+}
+
+void generate_def(ASTNode* tree, SymTable** table){
+    printf("LABEL $%s\n", tree->symbol->id);
+    printf("PUSHFRAME\n");
+
+    
+    //handle_next_block(tree, table);
+
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+}
+
 
 void generate_code(ASTNode* tree, SymTable **table, FILE* output){
 
